48dbeb7f9b86203193ca9a33691c54c9a60c05d9
Panagiotis Kotsias <kotsias.pan@gmail.com>
2019-09-17 10:18:48 +0200

    Initial commit.
    Initial commit of DDC repo as described in publication.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="ddc_pub",
+    version="0.1",
+    description="Neural network to encode/decode molecules.",
+    url="https://github.com/pcko1/Deep-Drug-Coder",
+    author="Panagiotis-Christos Kotsias",
+    author_email="kotsias.pan@gmail.com",
+    license="GPLv3",
+    packages=["ddc_pub"],
+    install_requires=[
+        "numpy",
+        "h5py",
+        "ipywidgets",
+        "tensorflow-gpu==1.14.0",
+        "keras",
+        "scikit-learn",
+        "scipy",
+        "ipykernel",
+        "ipython",
+        "matplotlib",
+        "pandas",
+    ],
+    zip_safe=False,
+)
